d3be10da891afe9e72d5f0fb4903f88e6ef6313a
ShoruSenpai <shorusenpai@gmail.com>
2025-10-08 16:09:53 +0700

    Update route

--- src/router.js
+++ src/router.js
@@ -1,20 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import home from './pages/home.vue'
-import about from './pages/about.vue'
-import product from './pages/product.vue'
-import contact from './pages/contact.vue'
-import partner from './pages/partner.vue'
+// import home from './pages/home.vue'
+// import about from './pages/about.vue'
+// import product from './pages/product.vue'
+// import contact from './pages/contact.vue'
+// import partner from './pages/partner.vue'
 
+// github route
 const routes = [
-  { path: '/', component: home, meta: { title: 'Tefa Bakery' } },
-  { path: '/Tentang-Kami', component: about, meta: { title: 'Tefa Bakery | Tentang Kami' } },
-  { path: '/Produk', component: product, meta: { title: 'Tefa Bakery | Produk' } },
-  { path: '/Kontak', component: contact, meta: { title: 'Tefa Bakery | Kontak' } },
-  { path: '/Kemitraan', component: partner, meta: { title: 'Tefa Bakery | Kemitraan' } },
+  { path: '/', component: () => import('./pages/home.vue'), meta: { title: 'Tefa Bakery' } },
+  {
+    path: '/Tentang-Kami',
+    component: () => import('./pages/about.vue'),
+    meta: { title: 'Tefa Bakery | Tentang Kami' },
+  },
+  {
+    path: '/Produk',
+    component: () => import('./pages/product.vue'),
+    meta: { title: 'Tefa Bakery | Produk' },
+  },
+  {
+    path: '/Kontak',
+    component: () => import('./pages/contact.vue'),
+    meta: { title: 'Tefa Bakery | Kontak' },
+  },
+  {
+    path: '/Kemitraan',
+    component: () => import('./pages/partner.vue'),
+    meta: { title: 'Tefa Bakery | Kemitraan' },
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
@@ -29,7 +46,16 @@ const router = createRouter({
 })
 
 router.afterEach((to) => {
-  document.title = to.meta.title
+  document.title = to.meta.title || 'Tefa Bakery'
 })
 
 export default router
+
+// Default route
+// const routes = [
+//   { path: '/', component: home, meta: { title: 'Tefa Bakery' } },
+//   { path: '/Tentang-Kami', component: about, meta: { title: 'Tefa Bakery | Tentang Kami' } },
+//   { path: '/Produk', component: product, meta: { title: 'Tefa Bakery | Produk' } },
+//   { path: '/Kontak', component: contact, meta: { title: 'Tefa Bakery | Kontak' } },
+//   { path: '/Kemitraan', component: partner, meta: { title: 'Tefa Bakery | Kemitraan' } },
+// ]
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Tefa-Bakery/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
