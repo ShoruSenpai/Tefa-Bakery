acf305ba1561ea842eced453199ef2ce44066c5d
ShoruSenpai <shorusenpai@gmail.com>
2025-10-12 17:02:24 +0700

    Update build route

--- dist/assets/contact-Cr8qkiu1.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as e,o}from"./index-C5ch4Z3L.js";const t={};function r(n,a){return o(),e("div")}const f=c(t,[["render",r]]);export{f as default};
